ab75efe226ed4627960196dd28f25746e45c440d
kara <89068705+gongitsune@users.noreply.github.com>
2024-02-21 22:00:54 +0900

    feat: fish pure setting

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,5 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set --universal pure_show_system_time true
-set --universal pure_color_system_time pure_color_mute
+# pure settings
+set pure_show_system_time true
+set pure_color_system_time 858585
+set pure_color_git_branch 858585
+
+
