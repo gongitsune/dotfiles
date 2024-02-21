3c60c2e6e370acef67cfe7009e6ecc41bf7ca680
kara <89068705+gongitsune@users.noreply.github.com>
2024-02-21 17:59:20 +0900

    feat: install pure in fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set --universal pure_show_system_time true
+set --universal pure_color_system_time pure_color_mute
