1d943453711d2fb13b77955a5155551f1ecdef47
kara <89068705+gongitsune@users.noreply.github.com>
2024-03-24 19:13:09 +0900

    Update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,10 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# pure settings
-set pure_show_system_time true
-set pure_color_system_time 858585
-set pure_color_git_branch 858585
+# starship settings
+starship init fish | source
 
 # ghcup and cabal
 fish_add_path $HOME/.cabal/bin
