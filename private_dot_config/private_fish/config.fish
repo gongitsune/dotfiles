ed6a43418ee7e80a3c05e2fb0f9336d44b930b4f
kara <89068705+gongitsune@users.noreply.github.com>
2024-04-09 23:20:12 +0900

    feat: update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -19,3 +19,5 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/keigo/.ghcup/bin $PATH # ghcup-env
